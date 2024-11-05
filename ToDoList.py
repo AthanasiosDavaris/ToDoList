579cf55b8f124230d9972503460e656875d151b8
Thanos Davaris <davthn08@gmail.com>
2024-11-05 23:28:28 +0200

    end of first day +

--- ToDoList.py
+++ ToDoList.py
@@ -1,1 +1,37 @@
-print("hello world!")
+import json
+from pathlib import Path
+from platform import uname
+
+
+# opens file and checks for errors
+try:
+  tasks = open("Tasks.json", "r+")
+except:
+  print("An unexpected error occured.")
+  # following code is in development
+  """
+  error ={
+    "ErrorType" : "FileError",
+    "FileName" : "Tasks.json",
+    "FilePath" : Path.cwd(),
+    "SysInfo" : uname()
+  }
+  with open("ErrorLog.json", "w") as outfile:
+    json.dump(error, outfile)
+  """
+
+
+# Start of the app and command prompt
+print("Welcome to your ToDo list!")
+while True:
+  command = input("What do you want ToDo? ").lower()
+
+  # Splits the command prompt
+  command_split = command.split()
+  if not command_split:
+    print("You just gave an empty command (not cool XXX)")
+    continue
+
+  # Checks the command type
+  if command_split[0] == "add":
+    pass
