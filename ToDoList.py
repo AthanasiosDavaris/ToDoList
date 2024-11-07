3db37dd0b8b100a42a05da96949bd2d952adae04
Thanos Davaris <davthn08@gmail.com>
2024-11-07 18:01:33 +0200

    day 2

--- ToDoList.py
+++ ToDoList.py
@@ -5,7 +5,7 @@ from platform import uname
 
 # opens file and checks for errors
 try:
-  tasks = open("Tasks.json", "r+")
+  tasks = open("Tasks.json", "a+")
 except:
   print("An unexpected error occured.")
   # following code is in development
@@ -31,21 +31,61 @@ while True:
   if not command_split:
     print("You just gave an empty command (not cool XXX)")
     continue
+  for word in command_split:
+    print(word)
 
   # Checks the command type
   if command_split[0] == "add":
     print("add")
+    try:
+      print("sheeesh")
+      command_split.remove("add")
+      tasks.write(s.replace('"', '') for s in command_split)
+      for word in command_split:
+        print(word)
+    except:
+      print("damn")
   elif command_split[0] == "update":
     print("update")
+    try:
+      pass
+    except:
+      pass
   elif command_split[0] == "delete":
     print("delete")
+    try:
+      pass
+    except:
+      pass
   elif command_split[0] == "quit":
     print("quit")
+    tasks.close()
+    exit()
+    try:
+      pass
+    except:
+      pass
   elif command_split[0] == "list":
     print("list")
+    try:
+      pass
+    except:
+      pass
   elif command_split[0] == "done_list":
     print("done_list")
+    try:
+      pass
+    except:
+      pass
   elif command_split[0] == "not_done_list":
     print("not_done_list")
+    try:
+      pass
+    except:
+      pass
   elif command_split[0] == "progress_list":
     print("progress_list")
+    try:
+      pass
+    except:
+      pass
